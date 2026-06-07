4e6f0cf57b0b0316f2d3075c993a89c776dceba1
WONJUN-KR <dnjswns0010@gmail.com>
2026-06-07 16:22:24 +0900

    chore: initial Spring Boot 4 scaffold (Java 21)

---
+++ src/test/java/com/wonjun/journeylog/JourneyLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjun.journeylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyLogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
